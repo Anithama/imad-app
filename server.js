cd1be16efabb2982eb0a29138dbad2c867a8821e
Anithama <annaanitha.09@gmail.com>
2017-08-20 11:53:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter);
+});
 app.get('/article1', function (req, res) {
   res.send("Hi this anitha and u r in article1");
 });
